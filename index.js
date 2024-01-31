8b7140328f03f0c9e817fee9252c573f8389e14c
Hanifc56 <abuhanif007sd@gmail.com>
2024-02-01 02:31:39 +0600

    got all data form mongodb

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const roomCollections = client
+      .db("hotelRooms")
+      .collection("RoomCollection");
+
+    app.get("/rooms", async (req, res) => {
+      const cursor = roomCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
